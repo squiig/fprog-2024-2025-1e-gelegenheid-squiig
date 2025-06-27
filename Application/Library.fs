6c24f367cadd5bafe9797dfb5fb22cd9c7915b39
Stan <8138029+squiig@users.noreply.github.com>
2025-06-27 16:49:08 +0200

    Remove unused 'Library' files & refactor solution into component folders

--- Application/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Application
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
--- DataAccess/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace DataAccess
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
