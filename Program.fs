58b3bb0a3f8781a2171434408925bb0d8d2a5304
Stan <8138029+squiig@users.noreply.github.com>
2025-07-16 17:02:31 +0200

    Fix Program.fs

--- Program.fs
+++ Program.fs
@@ -6,22 +6,22 @@ open Microsoft.Extensions.DependencyInjection
 open Giraffe
 
 open DrizzleCarton.Application
-open DrizzleCarton.DataAccess
-open DrizzleCarton.Service
+open DrizzleCarton.RAMDataAccess
+open DrizzleCarton.HTTPWebService
 
-let report<'T> (result: Result<'T, SimulatedDatabase.SimulatedDatabaseError>) =
+let report<'T> (result: Result<'T, RAMDataAccess.RAMDataAccessError>) =
   match result with
   | Ok n -> printfn "%A" n
   | Error exn -> printfn "%A" exn
 
 let databaseDemo () =
-  let db = SimulatedDatabase.connect ()
+  let db = RAMDataAccess.connect ()
 
-  match SimulatedDatabase.users db with
+  match RAMDataAccess.users db with
   | Ok users -> List.iter (fun u -> printfn "%A" u) users
   | Error exn -> printfn "%A" exn
 
-  report <| SimulatedDatabase.addUser db ("eleanor", 1024 * 1024 * 1024, 12)
+  report <| RAMDataAccess.addUser db ("eleanor", 1024 * 1024 * 1024, 12)
 
 /// Here routes are configured
 let webApp: HttpHandler = choose [ Web.webApp ]
@@ -30,7 +30,7 @@ let configureApp (app: IApplicationBuilder) = app.UseGiraffe webApp
 
 let configureServices (services: IServiceCollection) =
   // Add Giraffe dependencies
-  services.AddSingleton<SimulatedDatabase>(SimulatedDatabase.connect ()) |> ignore
+  //services.AddSingleton<RAMDataAccess>(RAMDataAccess.connect ()) |> ignore
   services.AddSingleton<IUserDataAccess>(User.userPersistence) |> ignore
   services.AddSingleton<IEntryDataAccess>(Entry.entryPersistence) |> ignore
   services.AddGiraffe() |> ignore
