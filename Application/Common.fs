4bba243eae1fac4fe43e7f3e9b7ff29e289cee53
Stan <8138029+squiig@users.noreply.github.com>
2025-07-17 18:57:38 +0200

    Rename Util to Common and add ValidationError type for later

--- Application/Common.fs
+++ Application/Common.fs
@@ -1,4 +1,6 @@
 module DrizzleCarton.Application.Util
 
+type ValidationError = ValidationError of string
+
 let resToOpt<'T, 'Error, 'U> (f: 'T -> 'U) (res: Result<Option<'T>, 'Error>) : Option<'U> =
   res |> Result.defaultValue None |> Option.map f
