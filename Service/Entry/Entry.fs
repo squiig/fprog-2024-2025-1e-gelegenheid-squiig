7f1faffca501b2f15f41e374c1eac7de90b43a00
Stan <8138029+squiig@users.noreply.github.com>
2025-08-21 23:37:01 +0200

    Inbetween commit for fixing entry web service

--- Service/Entry/Entry.fs
+++ Service/Entry/Entry.fs
@@ -1,33 +1,67 @@
 module DrizzleCarton.HTTPWebService.Entry
 
+open DrizzleCarton.Application.Entry
+open DrizzleCarton.Application.EntryRepositoryContract
+open DrizzleCarton.Application.User
+open DrizzleCarton.Application.UserRepositoryContract
+open DrizzleCarton.Model
+open DrizzleCarton.Model.Validation
+
 open Giraffe
 
-type EntryDTO = int * string * Option<int> * string * int
+type EntryDTO = int * string * int option * string * int
 
 type NestedEntriesDTO =
   { Entry: EntryDTO
-    Subentries: List<NestedEntriesDTO> }
+    SubEntries: NestedEntriesDTO option list option }
 
-let rec buildEntryResponse (db: Database) (entry: EntryDTO) : NestedEntriesDTO =
-  { Entry = entry
-    Subentries = Entry.fetchSubentries db entry.Id |> List.map (buildEntryResponse db) }
+let rec buildEntryDTO (entryRepo: IEntryRepository) (entry: Entry) : NestedEntriesDTO option =
+  let entryId, _, _, _, _ = Entry.toTuple entry
+  let entryDTO = Entry.toRawTuple entry
 
-let getAllEntries (userId: int) : HttpHandler =
+  match getSubEntries entryRepo entryId with
+  | SubEntriesFound subEntries ->
+    { Entry = entryDTO
+      SubEntries = subEntries |> List.map (buildEntryDTO entryRepo) |> Some }
+    |> Some
+  | ZeroSubEntries ->
+    { Entry = entryDTO
+      SubEntries = None }
+    |> Some
+  | GetSubEntriesResult.DataFailure _ -> None
+
+let getAllEntries (rawUserId: int) : HttpHandler =
   fun next ctx ->
     task {
-      let db = ctx.GetService<Database>()
+      let entryRepo = ctx.GetService<IEntryRepository>()
+      let userRepo = ctx.GetService<IUserRepository>()
 
-      match User.fetch db userId with
-      | Error _ -> return! RequestErrors.NOT_FOUND (sprintf "%i" userId) next ctx
-      | Ok user ->
-        match Entry.fetch db user.RootFolder with
-        | Error e ->
-          return!
-            RequestErrors.NOT_FOUND
-              (sprintf "Root folder for %s(%i) not found: %A" user.Username user.RootFolder e)
-              next
-              ctx
-        | Ok root ->
-          let response = buildEntryResponse db root
+      // validate user id
+      let userId =
+        match UserId.ofRaw rawUserId with
+        | Error (ValidationError msg) -> failwith msg // TODO: send proper failure response
+        | Ok id -> id
+
+      match userRepo.FindUserById userId with
+      //| Error _ -> return! RequestErrors.NOT_FOUND (sprintf "%i" userId) next ctx
+      | Error (ModelValidationError msg) -> failwith msg // TODO: send proper failure response
+      | Error (ReadUserFailure.DataAccessError msg) -> failwith msg // TODO: send proper failure response
+      | Ok None -> failwith msg // TODO: send proper failure response
+      | Ok (Some user) ->
+        let _,_,_,userRoot = User.toTuple user
+        let userRootId = UserRoot.toRaw userRoot
+
+        match entryRepo.FindEntryById userRootId with
+        //| Error e ->
+        //  return!
+        //    RequestErrors.NOT_FOUND
+        //      (sprintf "Root folder for %s(%i) not found: %A" user.Username user.RootFolder e)
+        //      next
+        //      ctx
+        | Error (ReadEntryFailure.ModelValidationError msg) -> failwith msg // TODO: send proper failure response
+        | Error (ReadEntryFailure.DataAccessError msg) -> failwith msg // TODO: send proper failure response
+        | Ok None -> failwith msg // TODO: send proper failure response
+        | Ok (Some root) ->
+          let response = buildEntryDTO entryRepo root
           return! json response next ctx
     }
