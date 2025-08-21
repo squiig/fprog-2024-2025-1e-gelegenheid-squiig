cf870350631e82b0ffd565a1fd7576ae5e9bae02
Stan <8138029+squiig@users.noreply.github.com>
2025-08-21 18:33:51 +0200

    Fix User web service

--- Service/User/User.fs
+++ Service/User/User.fs
@@ -2,7 +2,10 @@ module DrizzleCarton.HTTPWebService.User
 
 open DrizzleCarton.Model
 open DrizzleCarton.Application
+open DrizzleCarton.Application.Entry
+open DrizzleCarton.Application.EntryRepositoryContract
 open DrizzleCarton.Application.User
+open DrizzleCarton.Application.UserRepositoryContract
 
 open Giraffe
 
@@ -10,64 +13,107 @@ open Thoth.Json.Net
 
 let getAllUsers: HttpHandler =
   fun next ctx ->
-    let dataAccess = ctx.GetService<IUserDataAccess>()
+    let userRepo = ctx.GetService<IUserRepository>()
 
-    match getAll dataAccess with
-    | GetAllResult.DataAccessFailed ->
-      ServerErrors.serviceUnavailable
-        (text "Cannot process this request right now. Data access failed somehow.")
+    match getAll userRepo with
+    | GetAllResult.DataFailure msg ->
+      ServerErrors.INTERNAL_ERROR
+        $"Cannot process this request right now. Data could not be retrieved: %s{msg}"
         next
         ctx
     | GetAllResult.Found users -> json users next ctx
 
-let getUser (id: int) : HttpHandler =
+let getUser (rawId: int) : HttpHandler =
   fun next ctx ->
-    let dataAccess = ctx.GetService<IUserDataAccess>()
+    let userRepo = ctx.GetService<IUserRepository>()
 
-    match findById dataAccess (UserId.ofRaw id) with
-    | GetByIdResult.DataAccessFailed ->
-      ServerErrors.serviceUnavailable
-        (text "Cannot process this request right now. Data access failed somehow.")
-        next
-        ctx
-    | GetByIdResult.NotFound -> RequestErrors.notFound (text "No user found by this id") next ctx
-    | GetByIdResult.Found user -> json user next ctx
+    match UserId.ofRaw rawId with
+    | Error(Validation.ValidationError msg) -> RequestErrors.UNPROCESSABLE_ENTITY msg next ctx
+    | Ok userId ->
+      match findById userRepo userId with
+      | FindByIdResult.DataFailure msg ->
+        ServerErrors.INTERNAL_ERROR
+          $"Cannot process this request right now. Data could not be retrieved: %s{msg}"
+          next
+          ctx
+      | FindByIdResult.NotFound -> RequestErrors.NOT_FOUND "No user found by this id" next ctx
+      | FindByIdResult.Found user -> json user next ctx
 
-let renameUser (id: int) : HttpHandler =
+let renameUser (rawId: int) : HttpHandler =
   fun next ctx ->
     task {
-      let dataAccess = ctx.GetService<IUserDataAccess>()
+      let userRepo = ctx.GetService<IUserRepository>()
 
-      match dataAccess.FindUserById(UserId.ofRaw id) with
-      | Error _ -> return! RequestErrors.NOT_FOUND "User not found" next ctx
-      | Ok user ->
-        let! encodedName = ctx.ReadBodyFromRequestAsync false
+      match UserId.ofRaw rawId with
+      | Error(Validation.ValidationError msg) -> return! RequestErrors.UNPROCESSABLE_ENTITY msg next ctx
+      | Ok userId ->
+        match findById userRepo userId with
+        | FindByIdResult.DataFailure msg ->
+          return!
+            ServerErrors.INTERNAL_ERROR
+              $"Cannot process this request right now. Data could not be retrieved: %s{msg}"
+              next
+              ctx
+        | FindByIdResult.NotFound -> return! RequestErrors.NOT_FOUND "No user found by this id" next ctx
+        | FindByIdResult.Found oldUser ->
+          let! encodedName = ctx.ReadBodyFromRequestAsync false
 
-        match Decode.fromString Decode.string encodedName with
-        | Error e -> return! RequestErrors.BAD_REQUEST (sprintf "%A" e) next ctx
-        | Ok name ->
-          let newUser: User = { user with Username = name }
+          match Decode.fromString Decode.string encodedName with
+          | Error e -> return! RequestErrors.BAD_REQUEST (sprintf "%A" e) next ctx
+          | Ok decodedName ->
+            match UserName.ofRaw decodedName with
+            | Error(Validation.ValidationError msg) -> return! RequestErrors.UNPROCESSABLE_ENTITY msg next ctx
+            | Ok newUsername ->
+              let id, _, quota, root = User.toTuple oldUser
 
-          match User.update db id newUser with
-          | Ok user -> return! json user next ctx
-          | Error _ -> return! RequestErrors.BAD_REQUEST "Could not update user" next ctx
+              match User.make (id, newUsername, quota, root) with
+              | Error(Validation.ValidationError msg) ->
+                return!
+                  ServerErrors.INTERNAL_ERROR
+                    $"Unexpected user validation error when trying to rename user with id %i{rawId} to name %s{decodedName}. Message: %s{msg}"
+                    next
+                    ctx
+              | Ok dirtyUser ->
+                match User.update userRepo dirtyUser with
+                | Error(DataAccessError msg) ->
+                  return!
+                    ServerErrors.INTERNAL_ERROR
+                      $"Cannot process this request right now. Data access failed. Message: %s{msg}"
+                      next
+                      ctx
+                | Ok updatedUser -> return! json updatedUser next ctx
     }
 
-type CreateUserDTO = { Name: string; Quota: int }
+type CreateUserRequestDTO = { Name: string; Quota: int }
 
 let createUser: HttpHandler =
   fun next ctx ->
     task {
-      let dataAccess = ctx.GetService<IEntryDataAccess>()
+      let userRepo = ctx.GetService<IUserRepository>()
+      let entryRepo = ctx.GetService<IEntryRepository>()
       let! data = ctx.ReadBodyBufferedFromRequestAsync()
 
-      match Serialization.fromJson<CreateUserDTO> data with
+      match Decode.Auto.fromString<CreateUserRequestDTO> data with
       | Error e -> return! RequestErrors.BAD_REQUEST (sprintf "%s" e) next ctx
-      | Ok u ->
-        match dataAccess.StoreNewRootFolder with
-        | Ok entry ->
-          match User.add db u.Name u.Quota entry.Id with
-          | Ok user -> return! json user next ctx
-          | _ -> return! RequestErrors.BAD_REQUEST "failed to create user" next ctx
-        | _ -> return! RequestErrors.BAD_REQUEST "failed to create root folder" next ctx
+      | Ok userDTO ->
+        match Entry.storeNewRootFolder entryRepo with
+        | StoreResult.DataFailure msg ->
+          return!
+            ServerErrors.INTERNAL_ERROR
+              $"Aborting user creation! Failed to create root folder for new user. Message: %s{msg}"
+              next
+              ctx
+        | EntryStored rootFolder ->
+          let rootFolderId, _, _, _, _ = Entry.toTuple rootFolder
+
+          match User.add userRepo (userDTO.Name, userDTO.Quota, rootFolderId) with
+          | AddResult.DataAccessError msg ->
+            return!
+              ServerErrors.INTERNAL_ERROR
+                $"Cannot process this request right now. Data access failed. Message: %s{msg}"
+                next
+                ctx
+          | InvalidFieldError msg ->
+            return! RequestErrors.UNPROCESSABLE_ENTITY $"Could not create user, input data invalid: %s{msg}" next ctx
+          | Stored user -> return! json user next ctx
     }
