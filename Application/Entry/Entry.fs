a9ea62476f81ae715479abd852d9b94ae02ddbc4
Stan <8138029+squiig@users.noreply.github.com>
2025-08-23 18:52:52 +0200

    Fix Application.Entry

--- Application/Entry/Entry.fs
+++ Application/Entry/Entry.fs
@@ -7,38 +7,57 @@ open DrizzleCarton.Application.EntryRepositoryContract
 type FindByIdResult =
   | EntryFound of Entry
   | EntryNotFound
-  | DataFailure of Message
+  | DataRetrievingError of Message
 
-let findById (entryRepo: IEntryRepository) (id: EntryId) =
+let findById (entryRepo: IEntryRepository) id =
   match entryRepo.FindEntryById id with
-  | Error(ReadEntryFailure.DataAccessError s) -> DataFailure s
-  | Error(ModelValidationError s) ->
-    DataFailure(
-      Message
-        $"Illegal state: Entry with id %d{EntryId.toRaw id} could not be validated when read from storage! Message: '%s{s}'"
-    )
-  | Ok(Some entry) -> EntryFound entry
-  | Ok None -> EntryNotFound
+    | Error(ReadEntryFailure.DataAccessError msg) -> DataRetrievingError msg
+    | Error(ModelValidationError msg) ->
+      DataRetrievingError(
+        Message
+          $"Illegal state: Entry with id %d{(EntryId.toRaw id)} could not be validated when read from storage! '%s{msg}'"
+      )
+    | Ok(Some entry) -> EntryFound entry
+    | Ok None -> EntryNotFound
+
+type FindByRawIdResult =
+  | EntryFound of Entry
+  | EntryNotFound
+  | InvalidIdError of Message
+  | DataRetrievingError of Message
+
+let findByRawId (entryRepo: IEntryRepository) rawId =
+  match EntryId.ofRaw rawId with
+  | Error(Validation.ValidationError msg) -> InvalidIdError msg
+  | Ok id ->
+    match findById entryRepo id with
+    | FindByIdResult.DataRetrievingError msg -> DataRetrievingError msg
+    | FindByIdResult.EntryNotFound -> EntryNotFound
+    | FindByIdResult.EntryFound entry -> EntryFound entry
 
 type GetSubEntriesResult =
   | SubEntriesFound of Entry list
   | ZeroSubEntries
-  | DataFailure of Message
+  | InvalidIdError of Message
+  | DataRetrievingError of Message
 
-let getSubEntries (entryRepo: IEntryRepository) (id: EntryId) : GetSubEntriesResult =
-  match entryRepo.GetSubEntries id with
-  | Error(ReadEntryFailure.DataAccessError s) -> DataFailure s
-  | Error(ModelValidationError s) ->
-    DataFailure
-      $"Illegal state: One or more of the sub-entries of entry with id %d{EntryId.toRaw id} could not be validated when read from storage! Message: '%s{s}'"
-  | Ok subEntries when subEntries.IsEmpty -> ZeroSubEntries
-  | Ok subEntries -> SubEntriesFound subEntries
+let getSubEntries (entryRepo: IEntryRepository) rawId : GetSubEntriesResult =
+  match EntryId.ofRaw rawId with
+  | Error(Validation.ValidationError msg) -> InvalidIdError msg
+  | Ok id ->
+    match entryRepo.GetSubEntries id with
+    | Error(ReadEntryFailure.DataAccessError msg) -> DataRetrievingError msg
+    | Error(ModelValidationError msg) ->
+      DataRetrievingError
+        $"Illegal state: One or more of the sub-entries of entry with id %d{rawId} could not be validated when read from storage! Message: '%s{msg}'"
+    | Ok subEntries when subEntries.IsEmpty -> ZeroSubEntries
+    | Ok subEntries -> SubEntriesFound subEntries
 
 type GetParentResult =
   | ParentFound of Entry
   | NoParent
   | NonexistentParent of EntryId
-  | DataFailure of Message
+  | DataRetrievingError of Message
 
 let getParent (entryRepo: IEntryRepository) (entryData: EntryData) =
   let _, parent, _, _ = Entry.EntryData.toTuple entryData
@@ -47,9 +66,9 @@ let getParent (entryRepo: IEntryRepository) (entryData: EntryData) =
   | None -> NoParent
   | Some id ->
     match findById entryRepo id with
-    | FindByIdResult.DataFailure s -> DataFailure s
-    | EntryNotFound -> NonexistentParent id
-    | EntryFound e -> ParentFound e
+    | FindByIdResult.DataRetrievingError msg -> DataRetrievingError msg
+    | FindByIdResult.EntryNotFound -> NonexistentParent id
+    | FindByIdResult.EntryFound entry -> ParentFound entry
 
 module Validation =
   let maxLegalAncestors = 6
@@ -60,12 +79,12 @@ module Validation =
     | HasNonexistentAncestor of
       {| AncestorId: EntryId
          CountUntilAncestorExcluded: int |}
-    | DataFailure of Message
+    | DataRetrievingError of Message
 
   let countAncestors (entryRepo: IEntryRepository) (entryData: EntryData) : CountAncestorsResult =
     let rec count (counter: int) =
       match getParent entryRepo entryData with
-      | GetParentResult.DataFailure s -> DataFailure s
+      | GetParentResult.DataRetrievingError s -> DataRetrievingError s
       | NonexistentParent parentId ->
         HasNonexistentAncestor
           {| AncestorId = parentId
@@ -78,7 +97,7 @@ module Validation =
 
   let legalAncestorCount (entryRepo: IEntryRepository) invalid maxAncestorCount (entryData: EntryData) =
     match countAncestors entryRepo entryData with
-    | DataFailure s -> Error invalid
+    | DataRetrievingError _ -> Error invalid
     | HasNonexistentAncestor x when x.CountUntilAncestorExcluded >= maxAncestorCount -> Error invalid
     | HasNonexistentAncestor _ -> Ok entryData
     | ZeroAncestors -> Ok entryData
@@ -87,7 +106,7 @@ module Validation =
 
   let validateAncestors (entryRepo: IEntryRepository) (entry: EntryData) : Result<EntryData, ValidationError> =
     (match getParent entryRepo entry with
-     | GetParentResult.DataFailure s -> Error s
+     | GetParentResult.DataRetrievingError msg -> Error msg
      | NonexistentParent _ -> Error "Entry may not point to a parent that doesn't exist."
      | NoParent -> Ok entry
      | ParentFound parent ->
@@ -107,19 +126,19 @@ module Validation =
     entryData |> validateAncestors entryRepo |> Result.map (fun _ -> entryData) // Make sure to always return the original entry on successful validation
 
 type AddResult =
-  | DataAccessError of Message
+  | DataStoringError of Message
   | InvalidEntryError of Message
   | Stored of Entry
 
 let add (entryRepo: IEntryRepository) (name, parent, kind, size) =
-  match Entry.EntryData.make (name, parent, kind, size) with
+  match Entry.EntryData.ofRaw (name, parent, kind, size) with
   | Error(Validation.ValidationError msg) -> InvalidEntryError msg
   | Ok entryData ->
     match Validation.validate entryRepo entryData with
     | Error(Common.ValidationError msg) -> InvalidEntryError msg
     | Ok validatedEntryData ->
       match entryRepo.StoreEntry validatedEntryData with
-      | Error(WriteEntryFailure.DataAccessError msg) -> DataAccessError msg
+      | Error(WriteEntryFailure.DataAccessError msg) -> DataStoringError msg
       | Ok storedEntryId -> Entry.withId validatedEntryData storedEntryId |> Stored
 
 let storeNewRootFolder (entryRepo: IEntryRepository) =
