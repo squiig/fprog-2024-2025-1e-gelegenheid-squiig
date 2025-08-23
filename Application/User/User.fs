14b36b4a0741c29492935706671ef45b986db555
Stan <8138029+squiig@users.noreply.github.com>
2025-08-23 17:54:14 +0200

    Fix stuff in Application.User, change some names, add some comments, introduce Message type, etc.

--- Application/User/User.fs
+++ Application/User/User.fs
@@ -6,50 +6,74 @@ open DrizzleCarton.Application.UserRepositoryContract
 open DrizzleCarton.Application.EntryRepositoryContract
 
 type GetAllResult =
-  | DataFailure of string
+  | DataRetrievingError of Message
   | Found of User list
 
 let getAll (userRepo: IUserRepository) =
   match userRepo.GetAllUsers() with
-  | Error(ReadUserFailure.DataAccessError s) -> DataFailure s
-  | Error(ReadUserFailure.ModelValidationError s) -> DataFailure s
+  | Error(ReadUserFailure.DataAccessError s) -> DataRetrievingError s
+  | Error(ReadUserFailure.ModelValidationError s) -> DataRetrievingError s
   | Ok users -> Found users
 
 type FindByIdResult =
-  | DataFailure of string
+  | DataRetrievingError of Message
+  | InvalidIdError of Message
   | NotFound
   | Found of User
 
-let findById (userRepo: IUserRepository) (id: UserId) =
-  match userRepo.FindUserById id with
-  | Error(ReadUserFailure.ModelValidationError e) -> DataFailure e
-  | Error(ReadUserFailure.DataAccessError e) -> DataFailure e
-  | Ok None -> NotFound
-  | Ok(Some user) -> Found user
+let findById (userRepo: IUserRepository) rawId =
+  match UserId.ofRaw rawId with
+  | Error(Validation.ValidationError msg) -> InvalidIdError msg
+  | Ok userId ->
+    match userRepo.FindUserById userId with
+    | Error(ReadUserFailure.ModelValidationError e) -> DataRetrievingError e
+    | Error(ReadUserFailure.DataAccessError e) -> DataRetrievingError e
+    | Ok None -> NotFound
+    | Ok(Some user) -> Found user
 
 type AddResult =
-  | DataAccessError of string
-  | RootFolderError of string
-  | InvalidUserError of string
+  | RootFolderStoringError of Message
+  | UserDataStoringError of Message
+  | InvalidUserError of Message
   | Stored of User
 
 let add (userRepo: IUserRepository) (entryRepo: IEntryRepository) (rawUserName, rawUserQuota) =
   match Entry.storeNewRootFolder entryRepo with
-  | Entry.StoreResult.DataFailure msg -> RootFolderError msg
-  | Entry.EntryStored rootFolder ->
+  | Error(WriteEntryFailure.DataAccessError msg) -> RootFolderStoringError msg
+  | Ok rootFolder ->
+    // Try to combine everything into valid user data.
     match UserData.ofRaw (rawUserName, rawUserQuota, rootFolder) with
-    | Error(Validation.ValidationError msg) -> InvalidUserError msg
+    | Error(Validation.ValidationError msg) -> InvalidUserError msg // TODO: Make sure the root folder gets removed.
     | Ok userData ->
+      // Try to store the user data.
       match userRepo.StoreUser userData with
-      | Error(WriteUserFailure.DataAccessError msg) -> DataAccessError msg
+      | Error(WriteUserFailure.DataAccessError msg) -> UserDataStoringError msg
+      // If stored, assign the new id to the user data and return as a valid and stored User entity.
       | Ok storedUserId -> User.withId userData storedUserId |> Stored
 
-let update (userRepo: IUserRepository) user = userRepo.UpdateUser user
+type RenameResult =
+  | DataReadingError of Message
+  | DataWritingError of Message
+  | InvalidIdError of Message
+  | InvalidNameError of Message
+  | InvalidNameForUserError of Message
+  | UserNotFoundError
+  | UserUpdated of User
 
-module Validation =
+let rename (userRepo: IUserRepository) rawId rawNewName =
+  match findById userRepo rawId with
+  | FindByIdResult.DataRetrievingError msg -> DataReadingError msg
+  | FindByIdResult.InvalidIdError msg -> InvalidIdError msg
+  | FindByIdResult.NotFound -> UserNotFoundError
+  | FindByIdResult.Found oldUser ->
+    match UserName.ofRaw rawNewName with
+    | Error(Validation.ValidationError msg) -> InvalidNameError msg
+    | Ok newName ->
+      let id, _, quota, root = User.toTuple oldUser
 
-  let validate (entryRepo: IEntryRepository) (user: User) =
-    user
-    // Potential validations...
-    |> Ok
-    |> Result.map (fun _ -> user) // Whatever previous validations returned, return the input user if all succeeded
+      match User.make (id, newName, quota, root) with
+      | Error(Validation.ValidationError msg) -> InvalidNameForUserError msg
+      | Ok dirtyUser ->
+        match userRepo.UpdateUser dirtyUser with
+        | Error(WriteUserFailure.DataAccessError msg) -> DataWritingError msg
+        | Ok updatedUser -> UserUpdated updatedUser
