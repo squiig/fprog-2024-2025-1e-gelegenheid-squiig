e456290e0cb308578fd3922d0c54b81028a66753
Stan <8138029+squiig@users.noreply.github.com>
2025-08-23 19:24:18 +0200

    Make function private

--- Application/Entry/Entry.fs
+++ Application/Entry/Entry.fs
@@ -9,7 +9,7 @@ type FindByIdResult =
   | EntryNotFound
   | DataRetrievingError of Message
 
-let findById (entryRepo: IEntryRepository) id =
+let private findById (entryRepo: IEntryRepository) id =
   match entryRepo.FindEntryById id with
     | Error(ReadEntryFailure.DataAccessError msg) -> DataRetrievingError msg
     | Error(ModelValidationError msg) ->
