a804a6132159420cc16ac27a3ff516c06da30424
Stan <8138029+squiig@users.noreply.github.com>
2025-08-23 17:50:43 +0200

    Clarify Application.ValidationError strings are also messages

--- Application/Common.fs
+++ Application/Common.fs
@@ -1,6 +1,8 @@
 module DrizzleCarton.Application.Common
 
-type ValidationError = ValidationError of string
+type Message = string
+
+type ValidationError = ValidationError of Message
 
 let resToOpt<'T, 'Error, 'U> (f: 'T -> 'U) (res: Result<Option<'T>, 'Error>) : Option<'U> =
   res |> Result.defaultValue None |> Option.map f
