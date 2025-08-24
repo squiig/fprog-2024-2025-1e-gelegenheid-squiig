4549f0e581ee28452f988aec07169ee8ed81dd60
Stan <8138029+squiig@users.noreply.github.com>
2025-08-24 03:36:33 +0200

    Add GetAllUsers repo implementation

--- DataAccess/User/User.fs
+++ DataAccess/User/User.fs
@@ -7,6 +7,24 @@ open DrizzleCarton.Application.ResultHelper
 open DrizzleCarton.Model
 open DrizzleCarton.RAMDataAccess
 
+let mapUser db rawUser =
+  let id, name, quota, rootId = rawUser
+
+  match RAMDataAccess.entry db rootId with
+  | Ok rawRootEntry ->
+    match Entry.ofRaw rawRootEntry with
+    | Ok rootEntry ->
+      User.ofRaw (id, name, quota, rootEntry)
+      |> Result.mapError (function
+        | Validation.ValidationError msg -> ReadUserFailure.ModelValidationError msg)
+    | Error(Validation.ValidationError msg) ->
+      Error(ReadUserFailure.ModelValidationError $"User of id %d{id} has an invalid root folder! %s{msg}")
+  | Error(RAMDataAccessError.NotFound _) ->
+    Error(
+      ReadUserFailure.ModelValidationError
+        $"User (id %d{id}) found but invalid, the root folder id %d{rootId} does not exist!"
+    )
+
 let userPersistence: IUserRepository =
   { new IUserRepository with
 
@@ -15,24 +33,14 @@ let userPersistence: IUserRepository =
 
         match RAMDataAccess.user db (UserId.toRaw userId) with
         | Error(RAMDataAccessError.NotFound _) -> Ok None
-        | Ok rawUser ->
-          let id, name, quota, rootId = rawUser
-
-          match RAMDataAccess.entry db rootId with
-          | Ok rawRootEntry ->
-            match Entry.ofRaw rawRootEntry with
-            | Ok rootEntry ->
-              User.ofRaw (id, name, quota, rootEntry)
-              |> Result.map (fun u -> Some u)
-              |> Result.mapError (function
-                | Validation.ValidationError msg -> ReadUserFailure.ModelValidationError msg)
-            | Error(Validation.ValidationError msg) ->
-              Error(ReadUserFailure.ModelValidationError $"User has an invalid root folder! %s{msg}")
-          | Error(RAMDataAccessError.NotFound _) ->
-            Error(ReadUserFailure.ModelValidationError "User found but invalid, the root folder id does not exist!")
+        | Ok rawUser -> mapUser db rawUser |> Result.map (fun u -> Some u)
 
       member this.GetAllUsers() : Result<User list, ReadUserFailure> =
-        raise (System.NotImplementedException())
+        let db = RAMDataAccess.defaultDb
+
+        match RAMDataAccess.users db with
+        | Error(NotFound _) -> Ok []
+        | Ok rawUsers -> rawUsers |> List.map (mapUser db) |> sequenceResult
 
       member this.StoreUser(userData: UserData) : Result<UserId, WriteUserFailure> =
         raise (System.NotImplementedException())
