422cfbbdac5c0e0302c77eff773c23973dcecfba
Stan <8138029+squiig@users.noreply.github.com>
2025-08-24 02:40:23 +0200

    Remove unused entry contract functions & add failure cases

--- Application/Entry/RepositoryContract.fs
+++ Application/Entry/RepositoryContract.fs
@@ -5,16 +5,18 @@ open DrizzleCarton.Application.Common
 
 /// Any error that may come from the data access implementation when attempting to read Entries.
 type ReadEntryFailure =
-  | ModelValidationError of Message
   | DataAccessError of Message
+  | ModelValidationError of Message
+  | PermissionDenied
 
 /// Any error that may come from the data access implementation when attempting to write Entries.
-type WriteEntryFailure = DataAccessError of Message
+type WriteEntryFailure =
+  | DataAccessError of Message
+  | PermissionDenied
+  | UnexpectedResultError of Message
 
 /// Defines the data operations for Entry functionality to be implemented by some data access dependency.
 type IEntryRepository =
-  abstract GetAllEntries: unit -> Result<Entry list, ReadEntryFailure>
   abstract GetSubEntries: EntryId -> Result<Entry list, ReadEntryFailure>
   abstract FindEntryById: EntryId -> Result<Entry option, ReadEntryFailure>
   abstract StoreEntry: EntryData -> Result<EntryId, WriteEntryFailure>
-  abstract UpdateEntry: Entry -> Result<Entry, WriteEntryFailure>
